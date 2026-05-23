23994ab2101f5605cad785d9b77d24800eea09f1
Gabriel Costa Santos <jose.santos0758@academico.ifs.edu.br>
2026-05-23 00:05:39 -0300

    build: atualizei as rotas no App para integrar catalogo e detalhes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
 import { useState } from "react";
-import Home from "./pages/Home";
-import "./App.css";
 import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 import Login from "./pages/Login";
+import Catalogo from "./pages/catalogo/Catalogo";
+import ProductDetail from "./pages/product-detail/ProductDetail";
+import "./App.css";
 
 export default function App() {
   return (
     <Routes>
+      {/* Rota da Página Inicial (Home) */}
       <Route path="/" element={<Home />} />
-      <Route path="pages/login" element={<Login />} />
+
+      {/* Rota do Login (ajustada para ficar mais limpa) */}
+      <Route path="/login" element={<Login />} />
+
+      {/* Suas novas rotas */}
+      <Route path="/catalogo" element={<Catalogo />} />
+      <Route path="/produto/:id" element={<ProductDetail />} />
     </Routes>
   );
 }
